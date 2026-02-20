eb92b4401fac5fab6957baa30a6e276fcee742e5
anees al-najjar <alnajjaram@ornl.gov>
2026-02-20 14:57:16 -0500

    update "test_metadata_manager.py"

--- tests/test_metadata_manager.py
+++ tests/test_metadata_manager.py
@@ -3,3 +3,5 @@ from amscrot.controller.metadata_manager import MetadataManager
 #mtd=MetadataManager.fetch()
 mtd=MetadataManager.fetch(metadata_fetch_mode='local', metadata_id='service_client_metadata')
 print(mtd)
+
+# (.venv) PS C:\Users\4ua\Projects\amsc-isro-toolkit> python tests/test_metadata_manager.py
